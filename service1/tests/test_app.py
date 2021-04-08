ac982806bc292aa7051d4ac194838a625fb9e8ca
Oliver Oakley <olly@master-machine.europe-west2-b.c.mimetic-planet-305110.internal>
2021-04-08 14:05:42 +0000

    tests

--- service1/tests/test_app.py
+++ service1/tests/test_app.py
@@ -1,3 +1,19 @@
 from flask import url_for
 from flask_testing import TestCase
+from application import db
+from application.models import Prizes
 from app import app
+
+class TestBase(TestCase):
+    def create_app(self):
+        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///",
+            SECRET_KEY='TEST_SECRET_KEY',
+            DEBUG=True,
+            WTF_CSRF_ENABLED=False
+            )
+        return app
+
+class TestPrizeGenerator(TestBase):
+    def test_prize_generator(self):
+        response = self.client.get(url_for('prizegenerator'), follow_redirects =True)
+        self.assertEqual(response.status_code, 200)
--- service2/tests/test_app.py
+++ service2/tests/test_app.py
@@ -1,3 +1,17 @@
 from flask import url_for
 from flask_testing import TestCase
 from app import app
+
+class TestBase(TestCase):
+    def create_app(self):
+        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///",
+            SECRET_KEY='TEST_SECRET_KEY',
+            DEBUG=True,
+            WTF_CSRF_ENABLED=False
+            )
+        return app
+
+class TestService2(TestBase):
+    def test_service_2(self):
+        response = self.client.get(url_for('service2'), follow_redirects =True)
+        self.assertEqual(response.status_code, 200)
