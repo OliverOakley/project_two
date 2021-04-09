02184d584164b5d3423105b2cb99fb3d4162626e
Oliver Oakley <olly@master-machine.europe-west2-b.c.mimetic-planet-305110.internal>
2021-04-09 13:22:40 +0000

    prayer2

--- service1/application/__init__.py
+++ service1/application/__init__.py
@@ -4,9 +4,8 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-app.config['SECRET_KEY'] = '42069'
 db = SQLAlchemy(app) 
 
 from application import routes
