939127f6aaacdab4848379a7d877d1a4f4223090
Oliver Oakley <olly@master-machine.europe-west2-b.c.mimetic-planet-305110.internal>
2021-04-09 12:14:41 +0000

    deploy3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-scp -i ~/.shh/ansible_id_rsa docker-compose.yaml jenkins@master-machine:/home/olly/project_two/docker.compose.yaml
-ssh -i ~/.ssh/ansible_id_rsa jenkins@master-machine << EOF
-    export DATABASE_URI=${DATABASE_URI}
-    docker stack deploy --compose-file /home/olly/project_two/docker.compose.yaml todo-stack
-EOF
+# scp -i ~/.shh/ansible_id_rsa docker-compose.yaml jenkins@master-machine:/home/olly/project_two/docker.compose.yaml
+# ssh -i ~/.ssh/ansible_id_rsa jenkins@master-machine << EOF
+#     export DATABASE_URI=${DATABASE_URI}
+#     docker stack deploy --compose-file /home/olly/project_two/docker.compose.yaml todo-stack
+# EOF
+
+docker stack deploy --compose-file docker.compose.yaml 
