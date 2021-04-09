f8a18f23e088fcbe2035b40bc08394bafd0274fe
Oliver Oakley <olly@master-machine.europe-west2-b.c.mimetic-planet-305110.internal>
2021-04-09 13:12:54 +0000

    prayer

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
-scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@master-machine:/home/jenkins/docker.compose.yaml
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@master-machine:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/ansible_id_rsa jenkins@master-machine << EOF
     export DATABASE_URI=${DATABASE_URI}
-    docker stack deploy --compose-file /home/jenkins/docker.compose.yaml prizegenerator-stack
+    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml prizegenerator-stack
 EOF
