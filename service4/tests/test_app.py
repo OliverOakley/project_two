9619c94f633466faafec272aa597e473f123dc16
Oliver Oakley <olly@master-machine.europe-west2-b.c.mimetic-planet-305110.internal>
2021-04-10 11:09:35 +0000

    test6

--- service4/tests/test_app.py
+++ service4/tests/test_app.py
@@ -1,10 +1,18 @@
 from flask import url_for
 from flask_testing import TestCase
 from app import app
+import requests
 
 class TestBase(TestCase):
     def create_app(self):
         return app
+
+class TestGetRequests(TestBase):
+    def test_get_requests(self):
+        with patch('requests.get') as g:
+            response = self.client.get(url_for("test_get_requests"))
+            g.return_value.text = "test"
+            self.assertIn:(b'77', response.data)
 
 class TestService4(TestBase):
     def test_service_4(self):
